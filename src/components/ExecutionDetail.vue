07acd0daa3cec805e964c149a0e62e10ebf5de8c
jact <jachavar@gmail.com>
2021-05-03 10:22:34 +0200

    Fixed showDate method

--- src/components/ExecutionDetail.vue
+++ src/components/ExecutionDetail.vue
@@ -30,14 +30,7 @@ export default {
   },
   methods: {
     showDate(isoString, format = '') {
-      if (!format) {
-        format =
-          typeof isoString === 'string' && !isoString.includes('T')
-            ? 'YYYY-MM-DD'
-            : 'YYYY-MM-DD HH:mm:ss'
-      }
-
-      return date.formatDate(Date.parse(isoString), format)
+      return date.formatDate(Date.parse(isoString), 'YYYY-MM-DD HH:mm:ss')
     }
   }
 }
