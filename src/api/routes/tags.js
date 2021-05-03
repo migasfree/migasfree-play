424b353cb54d1e0753cde19507bf7d8c64577467
jact <jachavar@gmail.com>
2021-05-03 10:24:55 +0200

    Added tags routes

---
+++ src/api/routes/tags.js
@@ -0,0 +1,38 @@
+let { PythonShell } = require('python-shell')
+const express = require('express')
+
+const router = express.Router()
+
+PythonShell.defaultOptions = {
+  pythonPath: '/usr/bin/python3'
+}
+
+router.get('/available', (req, res) => {
+  const code = `
+import json
+from migasfree_client.tags import MigasFreeTags
+
+print(json.dumps(MigasFreeTags().get_available_tags()))`
+
+  PythonShell.runString(code, null, (err, results) => {
+    if (err) throw err
+    res.setHeader('Content-Type', 'application/json')
+    res.send(results[0])
+  })
+})
+
+router.get('/assigned', (req, res) => {
+  const code = `
+import json
+from migasfree_client.tags import MigasFreeTags
+
+print(json.dumps(MigasFreeTags().get_assigned_tags()))`
+
+  PythonShell.runString(code, null, (err, results) => {
+    if (err) throw err
+    res.setHeader('Content-Type', 'application/json')
+    res.send(results[0])
+  })
+})
+
+module.exports = router
