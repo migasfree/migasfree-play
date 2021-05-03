6629542b3450566a7f62567408a6f3212086018c
jact <jachavar@gmail.com>
2021-05-03 11:00:47 +0200

    Improved layout

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,17 @@
   <div id="q-app">
     <div
       v-if="$store.state.ui.isLoading"
-      class="window-height window-width row justify-center items-center content-center"
+      class="row window-height window-width justify-center items-center content-center"
     >
-      <q-spinner color="primary" size="8em" />
-      <div>{{ $gettext('Loading data...') }}</div>
+      <div class="col-12 text-center q-ma-md">
+        <img id="logo" src="img/migasfree-play.svg" width="200" />
+      </div>
+
+      <div class="col-12 text-center">
+        <q-spinner color="primary" size="8em" />
+      </div>
+
+      <div class="col-12 text-center">{{ $gettext('Loading data...') }}</div>
     </div>
 
     <router-view v-else />
