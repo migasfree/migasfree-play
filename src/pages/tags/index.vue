a7ceb5b207fa6b0f360ba4ac49bca1f19ddef564
jact <jachavar@gmail.com>
2021-05-03 10:26:57 +0200

    Added tags page

---
+++ src/pages/tags/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page padding>
+    <q-breadcrumbs>
+      <q-breadcrumbs-el :label="$gettext('Tags')" icon="mdi-tag" />
+    </q-breadcrumbs>
+
+    <Tags />
+  </q-page>
+</template>
+
+<script>
+import Tags from 'components/Tags'
+
+export default {
+  meta() {
+    return {
+      title: this.$gettext('Tags')
+    }
+  },
+  components: {
+    Tags
+  }
+}
+</script>
