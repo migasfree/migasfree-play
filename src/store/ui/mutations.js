1405dc1b7d71a79e5bbbab65121bc9817c8d4b73
jact <jachavar@gmail.com>
2021-05-05 07:52:03 +0200

    Added updating, updatingFinished mutations

--- src/store/ui/mutations.js
+++ src/store/ui/mutations.js
@@ -2,6 +2,14 @@ export function loading(state) {
   state.isLoading = true
 }
 
-export function finished(state) {
+export function loadingFinished(state) {
   state.isLoading = false
+}
+
+export function updating(state) {
+  state.isUpdating = true
+}
+
+export function updatingFinished(state) {
+  state.isUpdating = false
 }
