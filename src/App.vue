d0816fc6a620384be71d8ccae94379b353315b54
jact <jachavar@gmail.com>
2021-05-07 08:40:48 +0200

    Added banner if stopped App

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,23 @@
         <img id="logo" src="img/migasfree-play.svg" width="200" />
       </div>
 
-      <div class="col-12 text-center">
+      <div v-if="!$store.getters['app/stoppedApp']" class="col-12 text-center">
         <q-spinner-clock color="primary" size="6em" />
       </div>
 
       <div class="col-12 text-center q-mt-md">
         {{ $gettext('Loading data') }}: {{ $store.getters['app/status'] }}
       </div>
+
+      <q-banner
+        v-if="$store.getters['app/stoppedApp']"
+        class="text-white bg-red q-ma-md"
+      >
+        <template #avatar>
+          <q-icon name="mdi-alert-octagon" />
+        </template>
+        <div class="text-h6">{{ $gettext('Impossible to continue.') }}</div>
+      </q-banner>
     </div>
 
     <router-view v-else />
