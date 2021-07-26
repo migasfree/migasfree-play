8f71022886011fe08de2e30b9d0290452daeee64
jact <jachavar@gmail.com>
2021-07-26 12:32:04 +0200

    Upgraded project to Quasar2

--- src/store/executions/mutations.js
+++ src/store/executions/mutations.js
@@ -1,8 +1,7 @@
-import Vue from 'vue'
 import { date } from 'quasar'
 import { executionsMaxLength } from 'config/app.conf'
 
-const { app } = require('electron').remote
+const app = window.electronRemote.app // electron-preload.js
 
 export function setExecutionsLog(state, value) {
   state.log = value
@@ -10,23 +9,23 @@ export function setExecutionsLog(state, value) {
     state.lastId = Object.keys(value)[Object.keys(value).length - 1]
 }
 
-export function startedCmd(state) {
+export async function startedCmd(state) {
   state.isRunningCommand = true
   app.canExit = false
 }
 
-export function finishedCmd(state) {
+export async function finishedCmd(state) {
   state.isRunningCommand = false
   app.canExit = true
 }
 
 export function addExecution(state, { command, icon }) {
   state.lastId = date.formatDate(Date.parse(new Date()), 'YYYY-MM-DD HH:mm:ss')
-  Vue.set(state.log, state.lastId, {
+  state.log[state.lastId] = {
     command,
     icon,
-    text: ''
-  })
+    text: '',
+  }
   while (Object.keys(state.log).length > executionsMaxLength)
     delete state.log[Object.keys(state.log)[0]]
 }
