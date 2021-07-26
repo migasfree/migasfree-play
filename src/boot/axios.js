b0c7aaacb882eadd0820b46cca087d3c33a2edc4
jact <jachavar@gmail.com>
2021-07-26 12:38:36 +0200

    Upgraded project to Quasar2

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
 export const cancelSource = axios.CancelToken.source()
@@ -6,49 +6,63 @@ export const cancelSource = axios.CancelToken.source()
 const axiosInstance = axios.create({
   paramsSerializer: (params) => {
     return qs.stringify(params, { arrayFormat: 'repeat' })
-  }
+  },
 })
 
-axiosInstance.interceptors.request.use(
-  (config) => {
-    /* const authToken = LocalStorage.getItem('auth.token')
+export default boot(({ app, store }) => {
+  axiosInstance.interceptors.request.use(
+    (config) => {
+      /* const authToken = store.getters['app/token']
 
-    if (authToken) {
-      config.headers.Authorization = `Token ${authToken}`
-    } */ // TODO
+      if (authToken) {
+        config.headers.Authorization = authToken
+      } */ // TODO
 
-    config.headers['Accept-Language'] = `${Vue.config.language.replace(
-      '_',
-      '-'
-    )},${Vue.config.language.split('_')[0]};q=0.9`
+      const language = store.getters['preferences/getLanguage']
 
-    // config.timeout = 10000
-    config.cancelToken = cancelSource.token
+      config.headers['Accept-Language'] = `${language.replace('_', '-')},${
+        language.split('_')[0]
+      };q=0.9`
 
-    console.log('[ REQUEST ]', config.url, config.params, config.headers)
+      // config.timeout = 10000
+      config.cancelToken = cancelSource.token
 
-    return config
-  },
+      console.log('[ REQUEST ]', config.url, config.params, config.headers)
 
-  (error) => {
-    return Promise.reject(error)
-  }
-)
+      return config
+    },
 
-axiosInstance.interceptors.response.use(
-  (response) => {
-    console.log('[ RESPONSE ]', response.config.url, response)
-    return response
-  },
+    (error) => {
+      return Promise.reject(error)
+    }
+  )
 
-  (error) => {
-    // TODO https://haxzie.com/architecting-http-clients-vue-js-network-layer
-    console.error(error.response.status, error.message)
-    return Promise.reject(error)
-  }
-)
+  axiosInstance.interceptors.response.use(
+    (response) => {
+      console.log('[ RESPONSE ]', response.config.url, response)
+      return response
+    },
 
-export default ({ store, Vue }) => {
-  Vue.prototype.$axios = axiosInstance
+    (error) => {
+      // TODO https://haxzie.com/architecting-http-clients-vue-js-network-layer
+      if ('response' in error)
+        console.error(error.response.status, error.message)
+      else console.error(error)
+      return Promise.reject(error)
+    }
+  )
+
+  // for use inside Vue files (Options API) through this.$axios and this.$api
+
+  app.config.globalProperties.$axios = axios
+  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
+  //       so you won't necessarily have to import axios in each vue file
+
+  app.config.globalProperties.$api = axiosInstance
+  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
+  //       so you can easily perform requests against your app's API
+
   store.$axios = axiosInstance
-}
+})
+
+export { axios, axiosInstance }
