05e9acbd7a66f8a7fb7fd85b2d0d31a9be8a74f6
jact <jachavar@gmail.com>
2021-07-28 13:24:45 +0200

    Upgraded Vue to version 3

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,158 +1,160 @@
 <template>
-  <div>
-    <div class="print-hide">
-      <div class="column items-center">
-        <p>
-          <img width="120" src="img/migasfree-play.svg" />
-        </p>
-
-        <p class="text-h5">
-          <strong>{{ appName }} {{ appVersion }}</strong>
-        </p>
-
-        <p class="text-caption text-center">
-          {{ appDescription }} <br />{{ appCopyright }} <br />{{ appAuthors }}
-        </p>
-
-        <q-card v-if="computer.user" flat bordered class="half q-ma-md">
-          <q-card-section>
-            <div class="text-h6 q-mb-md">
-              <q-icon name="mdi-account" />
-              {{ computer.user }}
-            </div>
-
-            <p>
-              <q-icon name="mdi-calendar-check" />
-              {{ syncEndDate }}
-            </p>
-
-            <q-tooltip>{{ $gettext('Last synchronization') }}</q-tooltip>
-          </q-card-section>
-        </q-card>
-
-        <q-card
-          v-if="'product' in computer.data"
-          flat
-          bordered
-          class="half q-ma-md"
-        >
-          <q-card-section>
-            <div class="text-h6 q-mb-md">
-              <q-icon :name="productIcon" />
-              {{ computer.data.product }}
-            </div>
-
-            <p>
-              <q-icon :name="cpuIcon" />
-              {{ computer.data.cpu }}
-            </p>
-
-            <p>
-              <q-icon name="mdi-memory" />
-              {{ computerRam }}
-            </p>
-
-            <p>
-              <q-icon name="mdi-harddisk" />
-              {{ computerStorage }}
-            </p>
-
-            <q-tooltip>{{ $gettext('Hardware') }}</q-tooltip>
-          </q-card-section>
-        </q-card>
-
-        <q-card
-          v-if="'mac_address' in computer.data"
-          flat
-          bordered
-          class="half q-ma-md"
-        >
-          <q-card-section>
-            <div class="text-h6 q-mb-md">
-              <q-icon name="mdi-information" />
-              {{ computer.data.fqdn }}
-            </div>
-
-            <p>
-              <q-icon name="mdi-ip-network" />
-              {{ computer.data.ip_address }}
-              / {{ computer.mask }} ({{ computer.network }})
-            </p>
-
-            <p>
-              <q-icon name="mdi-swap-vertical" />
-              {{ computerMac }}
-            </p>
-
-            <q-tooltip>{{ $gettext('Network Data') }}</q-tooltip>
-          </q-card-section>
-        </q-card>
-
-        <q-card flat bordered class="half q-ma-md">
-          <q-card-section>
-            <div class="text-h6 q-mb-md">
-              <q-icon name="mdi-server" />
-              {{ $store.getters['app/host'] }}
-            </div>
-
-            <p>
-              <q-icon name="mdi-sitemap" />
-              {{ computer.project }}
-            </p>
-
-            <p>
-              <q-icon name="mdi-pound" />
-              {{ computerId }}
-            </p>
-
-            <p>
-              <q-icon :name="statusIcon" />
-              {{ statusText }}
-            </p>
-
-            <q-tooltip>{{ $gettext('Migasfree Data') }}</q-tooltip>
-          </q-card-section>
-        </q-card>
-      </div>
-    </div>
-
+  <div class="print-hide">
     <div class="column items-center">
-      <q-card flat bordered class="identification">
-        <q-card-section horizontal>
-          <vue-qrcode
-            :value="qrCode"
-            :options="{ width: 140, errorCorrectionLevel: 'low' }"
-          />
+      <p>
+        <img width="120" src="img/migasfree-play.svg" />
+      </p>
 
-          <q-card-section vertical class="justify-around q-px-md">
-            <p>{{ computer.name }} ({{ computerId }})</p>
-            <div class="text-caption text-blue-grey">
-              <p>{{ computer.uuid }}</p>
-              <p>{{ $store.getters['app/host'] }}</p>
-              <p>{{ computer.helpdesk }}</p>
-            </div>
-          </q-card-section>
+      <p class="text-h5">
+        <strong>{{ appName }} {{ appVersion }}</strong>
+      </p>
+
+      <p class="text-caption text-center">
+        {{ appDescription }} <br />{{ appCopyright }} <br />{{ appAuthors }}
+      </p>
+
+      <q-card v-if="computer.user" flat bordered class="half q-ma-md">
+        <q-card-section>
+          <div class="text-h6 q-mb-md">
+            <q-icon name="mdi-account" />
+            {{ computer.user }}
+          </div>
+
+          <p>
+            <q-icon name="mdi-calendar-check" />
+            {{ syncEndDate }}
+          </p>
+
+          <q-tooltip>{{ $gettext('Last synchronization') }}</q-tooltip>
         </q-card-section>
+      </q-card>
 
-        <q-separator inset />
+      <q-card
+        v-if="'product' in computer.data"
+        flat
+        bordered
+        class="half q-ma-md"
+      >
+        <q-card-section>
+          <div class="text-h6 q-mb-md">
+            <q-icon :name="productIcon" />
+            {{ computer.data.product }}
+          </div>
 
-        <q-card-actions align="center" class="print-hide">
-          <q-btn
-            color="positive"
-            icon="mdi-printer"
-            class="q-mx-lg"
-            @click="printLabel"
-          >
-            <q-tooltip>{{ $gettext('Print Identification') }}</q-tooltip>
-          </q-btn>
-        </q-card-actions>
+          <p>
+            <q-icon :name="cpuIcon" />
+            {{ computer.data.cpu }}
+          </p>
+
+          <p>
+            <q-icon name="mdi-memory" />
+            {{ computerRam }}
+          </p>
+
+          <p>
+            <q-icon name="mdi-harddisk" />
+            {{ computerStorage }}
+          </p>
+
+          <q-tooltip>{{ $gettext('Hardware') }}</q-tooltip>
+        </q-card-section>
+      </q-card>
+
+      <q-card
+        v-if="'mac_address' in computer.data"
+        flat
+        bordered
+        class="half q-ma-md"
+      >
+        <q-card-section>
+          <div class="text-h6 q-mb-md">
+            <q-icon name="mdi-information" />
+            {{ computer.data.fqdn }}
+          </div>
+
+          <p>
+            <q-icon name="mdi-ip-network" />
+            {{ computer.data.ip_address }}
+            / {{ computer.mask }} ({{ computer.network }})
+          </p>
+
+          <p>
+            <q-icon name="mdi-swap-vertical" />
+            {{ computerMac }}
+          </p>
+
+          <q-tooltip>{{ $gettext('Network Data') }}</q-tooltip>
+        </q-card-section>
+      </q-card>
+
+      <q-card flat bordered class="half q-ma-md">
+        <q-card-section>
+          <div class="text-h6 q-mb-md">
+            <q-icon name="mdi-server" />
+            {{ $store.getters['app/host'] }}
+          </div>
+
+          <p>
+            <q-icon name="mdi-sitemap" />
+            {{ computer.project }}
+          </p>
+
+          <p>
+            <q-icon name="mdi-pound" />
+            {{ computerId }}
+          </p>
+
+          <p>
+            <q-icon :name="statusIcon" />
+            {{ statusText }}
+          </p>
+
+          <q-tooltip>{{ $gettext('Migasfree Data') }}</q-tooltip>
+        </q-card-section>
       </q-card>
     </div>
+  </div>
+
+  <div class="column items-center">
+    <q-card flat bordered class="identification">
+      <q-card-section horizontal>
+        <vue-qrcode
+          :value="qrCode"
+          :options="{ width: 140, errorCorrectionLevel: 'low' }"
+        />
+
+        <q-card-section vertical class="justify-around q-px-md">
+          <p>{{ computer.name }} ({{ computerId }})</p>
+          <div class="text-caption text-blue-grey">
+            <p>{{ computer.uuid }}</p>
+            <p>{{ $store.getters['app/host'] }}</p>
+            <p>{{ computer.helpdesk }}</p>
+          </div>
+        </q-card-section>
+      </q-card-section>
+
+      <q-separator inset />
+
+      <q-card-actions align="center" class="print-hide">
+        <q-btn
+          color="positive"
+          icon="mdi-printer"
+          class="q-mx-lg"
+          @click="printLabel"
+        >
+          <q-tooltip>{{ $gettext('Print Identification') }}</q-tooltip>
+        </q-btn>
+      </q-card-actions>
+    </q-card>
   </div>
 </template>
 
 <script>
+import { ref, computed } from 'vue'
+import { useStore } from 'vuex'
+import { useGettext } from '@jshmrtn/vue3-gettext'
 import { date } from 'quasar'
+
 import VueQrcode from '@chenfengyuan/vue-qrcode'
 
 const app = require('../../package.json')
@@ -162,63 +164,61 @@ export default {
   components: {
     VueQrcode,
   },
-  data() {
-    return {
-      appName: app.name,
-      appVersion: app.version,
-      appDescription: app.description,
-      appAuthors: app.author,
-      appCopyright: app.copyright,
-    }
-  },
-  computed: {
-    computer() {
-      return this.$store.getters['computer/getComputer']
-    },
+  setup() {
+    const store = useStore()
+    const { $gettext } = useGettext()
 
-    syncEndDate() {
-      return 'sync_end_date' in this.computer.data
+    const appName = ref(app.name)
+    const appVersion = ref(app.version)
+    const appDescription = ref(app.description)
+    const appAuthors = ref(app.author)
+    const appCopyright = ref(app.copyright)
+
+    const computer = computed(() => store.getters['computer/getComputer'])
+
+    const syncEndDate = computed(() =>
+      'sync_end_date' in computer.value.data
         ? date.formatDate(
-            Date.parse(this.computer.data.sync_end_date),
+            Date.parse(computer.value.data.sync_end_date),
             'YYYY-MM-DD HH:mm:ss'
           )
         : ''
-    },
+    )
 
-    computerRam() {
-      return 'ram' in this.computer.data
-        ? `${this.bytesToGigas(this.computer.data.ram)} GB RAM`
+    const computerRam = computed(() =>
+      'ram' in computer.value.data
+        ? `${bytesToGigas(computer.value.data.ram)} GB RAM`
         : ''
-    },
+    )
 
-    computerStorage() {
-      return 'storage' in this.computer.data
-        ? `${this.bytesToGigas(this.computer.data.storage)} GB (${
-            this.computer.data.disks
-          } ${this.$gettext('disks')})`
+    const computerStorage = computed(() =>
+      'storage' in computer.value.data
+        ? `${bytesToGigas(computer.value.data.storage)} GB (${
+            computer.value.data.disks
+          } ${$gettext('disks')})`
         : ''
-    },
+    )
 
-    computerMac() {
+    const computerMac = computed(() => {
       const ret = []
 
-      if (this.computer.data.mac_address) {
+      if (computer.value.data.mac_address) {
         let tmp = ''
-        for (let i = 0; i < this.computer.data.mac_address.length; i += 12) {
-          tmp = this.computer.data.mac_address.substring(i, i + 12)
+        for (let i = 0; i < computer.value.data.mac_address.length; i += 12) {
+          tmp = computer.value.data.mac_address.substring(i, i + 12)
           ret.push(tmp.replace(/(.{2})/g, '$1:').slice(0, -1))
         }
       }
 
       return ret.join(', ')
-    },
+    })
 
-    computerId() {
-      return this.computer.cid ? `CID-${this.computer.cid}` : 'CID-?'
-    },
+    const computerId = computed(() =>
+      computer.value.cid ? `CID-${computer.value.cid}` : 'CID-?'
+    )
 
-    productIcon() {
-      switch (this.computer.data.product_system) {
+    const productIcon = computed(() => {
+      switch (computer.value.data.product_system) {
         case 'desktop':
           return 'mdi-desktop-tower-monitor'
         case 'laptop':
@@ -230,14 +230,14 @@ export default {
         default:
           return 'mdi-help'
       }
-    },
+    })
 
-    cpuIcon() {
-      return `mdi-cpu-${this.computer.data.architecture}-bit`
-    },
+    const cpuIcon = computed(
+      () => `mdi-cpu-${computer.value.data.architecture}-bit`
+    )
 
-    statusIcon() {
-      switch (this.computer.data.status) {
+    const statusIcon = computed(() => {
+      switch (computer.value.data.status) {
         case 'available':
           return 'mdi-cart'
         case 'in repair':
@@ -251,43 +251,62 @@ export default {
         default:
           return 'mdi-crosshairs-question'
       }
-    },
+    })
 
-    statusText() {
-      switch (this.computer.data.status) {
+    const statusText = computed(() => {
+      switch (computer.value.data.status) {
         case 'available':
-          return this.$gettext('Available')
+          return $gettext('Available')
         case 'in repair':
-          return this.$gettext('In repair')
+          return $gettext('In repair')
         case 'reserved':
-          return this.$gettext('Reserved')
+          return $gettext('Reserved')
         case 'intended':
-          return this.$gettext('Intended')
+          return $gettext('Intended')
         case 'unsubscribed':
-          return this.$gettext('Unsubscribed')
+          return $gettext('Unsubscribed')
         default:
-          return this.$gettext('Unknown')
+          return $gettext('Unknown')
       }
-    },
+    })
 
-    qrCode() {
+    const qrCode = computed(() => {
       let info = {
         model: 'computer',
-        id: this.computer.cid,
-        server: this.$store.getters['app/host'],
+        id: computer.value.cid,
+        server: store.getters['app/host'],
       }
 
       return JSON.stringify(info)
-    },
-  },
-  methods: {
-    bytesToGigas(value) {
-      return (value / 1024 / 1024 / 1024).toFixed(1)
-    },
+    })
 
-    printLabel() {
+    const bytesToGigas = (value) => {
+      return (value / 1024 / 1024 / 1024).toFixed(1)
+    }
+
+    const printLabel = () => {
       window.print()
-    },
+    }
+
+    return {
+      appName,
+      appVersion,
+      appDescription,
+      appAuthors,
+      appCopyright,
+      computer,
+      syncEndDate,
+      computerRam,
+      computerStorage,
+      computerMac,
+      computerId,
+      productIcon,
+      cpuIcon,
+      statusIcon,
+      statusText,
+      qrCode,
+      printLabel,
+    }
   },
 }
 </script>
