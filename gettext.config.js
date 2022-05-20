aa08df8fa6092ee82dfbda47c063857aa0ae344e
Jose Antonio Chavarría <jachavar@gmail.com>
2022-05-20 14:38:16 +0200

    Fixed gettext config

--- gettext.config.js
+++ gettext.config.js
@@ -5,12 +5,12 @@ module.exports = {
     exclude: [], // glob patterns to exclude files from extraction
   },
   output: {
-    path: './src/i18n', // output path of all created files
-    potPath: './app.pot', // relative to output.path
-    jsonPath: './translations.json', // relative to output.path
-    locales: ['en', 'es_ES'],
-    flat: false, // don't create subdirectories for locales
-    linguas: true, // create a LINGUAS file
+    path: './src/i18n',
+    potPath: './app.pot',
+    jsonPath: './translations.json',
+    locales: ['es_ES'],
+    flat: true,
+    linguas: false,
     splitJson: false, // create separate json files for each locale. If used, jsonPath must end with a directory, not a file
   },
 }
