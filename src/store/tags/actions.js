1466d8f241d10eb22f425bd42026fab5268c05e2
jact <jachavar@gmail.com>
2022-09-13 20:52:40 +0200

    Checked computer cid in actions

--- src/store/tags/actions.js
+++ src/store/tags/actions.js
@@ -1,23 +1,25 @@
 import { internalApi } from 'config/app.conf'
 
 export async function getAvailableTags(context) {
-  await this.$axios
-    .get(`${internalApi}/tags/available`)
-    .then((response) => {
-      context.commit('setAvailableTags', response.data)
-    })
-    .catch((error) => {
-      context.dispatch('ui/notifyError', error, { root: true })
-    })
+  if (context.rootState.computer.cid)
+    await this.$axios
+      .get(`${internalApi}/tags/available`)
+      .then((response) => {
+        context.commit('setAvailableTags', response.data)
+      })
+      .catch((error) => {
+        context.dispatch('ui/notifyError', error, { root: true })
+      })
 }
 
 export async function getAssignedTags(context) {
-  await this.$axios
-    .get(`${internalApi}/tags/assigned`)
-    .then((response) => {
-      context.commit('setAssignedTags', response.data)
-    })
-    .catch((error) => {
-      context.dispatch('ui/notifyError', error, { root: true })
-    })
+  if (context.rootState.computer.cid)
+    await this.$axios
+      .get(`${internalApi}/tags/assigned`)
+      .then((response) => {
+        context.commit('setAssignedTags', response.data)
+      })
+      .catch((error) => {
+        context.dispatch('ui/notifyError', error, { root: true })
+      })
 }
