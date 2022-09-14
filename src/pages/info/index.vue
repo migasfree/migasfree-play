0117ece1a205ecf27824c62bd86a8262a0d8cf52
Jose Antonio Chavarría <jachavar@gmail.com>
2022-09-14 10:19:14 +0200

    Added update button (and updateInfo method)

--- src/pages/info/index.vue
+++ src/pages/info/index.vue
@@ -2,13 +2,31 @@
   <q-page padding>
     <q-breadcrumbs class="print-hide">
       <q-breadcrumbs-el :label="$gettext('Info')" icon="info" />
+      <q-btn
+        icon="mdi-sync"
+        size="sm"
+        flat
+        color="primary"
+        :loading="$store.state.ui.isUpdating"
+        :disabled="$store.state.ui.isUpdating"
+        @click="updateInfo"
+      >
+        <q-tooltip>{{ $gettext('Update') }}</q-tooltip></q-btn
+      >
     </q-breadcrumbs>
 
-    <Info />
+    <div v-if="$store.state.ui.isUpdating" class="row q-ma-xl">
+      <div class="col-12 text-center">
+        <q-spinner color="primary" size="6em" />
+      </div>
+    </div>
+
+    <Info v-else />
   </q-page>
 </template>
 
 <script>
+import { useStore } from 'vuex'
 import { useGettext } from 'vue3-gettext'
 import { useMeta } from 'quasar'
 
@@ -19,9 +37,22 @@ export default {
     Info,
   },
   setup() {
+    const store = useStore()
     const { $gettext } = useGettext()
 
     useMeta({ title: $gettext('Info') })
+
+    const updateInfo = async () => {
+      store.commit('ui/updating')
+      await store.dispatch('computer/computerInfo')
+      await store.dispatch('computer/computerNetwork')
+      await store.dispatch('computer/computerId')
+      await store.dispatch('computer/computerData')
+      await store.dispatch('computer/computerAttribute')
+      store.commit('ui/updatingFinished')
+    }
+
+    return { updateInfo }
   },
 }
 </script>
