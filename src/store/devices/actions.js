37769545014a54dc75ffcf3b2faee19f31c6416f
Jose Antonio Chavarría <jachavar@gmail.com>
2022-09-14 08:25:12 +0200

    Checked computer cid in actions

--- src/store/devices/actions.js
+++ src/store/devices/actions.js
@@ -1,33 +1,35 @@
 import { tokenApi } from 'config/app.conf'
 
 export async function computerDevices(context) {
-  await this.$axios
-    .get(
-      `${context.rootState.app.initialUrl.token}${tokenApi.computer}${context.rootState.computer.cid}/devices/`,
-      { headers: { Authorization: context.rootState.app.tokenValue } }
-    )
-    .then((response) => {
-      context.commit('setComputerDevices', response.data)
-    })
-    .catch((error) => {
-      context.dispatch('ui/notifyError', error, { root: true })
-    })
+  if (context.rootState.computer.cid)
+    await this.$axios
+      .get(
+        `${context.rootState.app.initialUrl.token}${tokenApi.computer}${context.rootState.computer.cid}/devices/`,
+        { headers: { Authorization: context.rootState.app.tokenValue } }
+      )
+      .then((response) => {
+        context.commit('setComputerDevices', response.data)
+      })
+      .catch((error) => {
+        context.dispatch('ui/notifyError', error, { root: true })
+      })
 }
 
 export async function getAvailableDevices(context) {
   const computer = context.rootGetters['computer/getComputer']
 
-  await this.$axios
-    .get(
-      `${context.rootState.app.initialUrl.token}${tokenApi.availableDevices}${computer.cid}&page_size=${Number.MAX_SAFE_INTEGER}`,
-      { headers: { Authorization: context.rootState.app.tokenValue } }
-    )
-    .then((response) => {
-      context.commit('setAvailableDevices', response.data.results)
-    })
-    .catch((error) => {
-      context.dispatch('ui/notifyError', error, { root: true })
-    })
+  if (computer.cid)
+    await this.$axios
+      .get(
+        `${context.rootState.app.initialUrl.token}${tokenApi.availableDevices}${computer.cid}&page_size=${Number.MAX_SAFE_INTEGER}`,
+        { headers: { Authorization: context.rootState.app.tokenValue } }
+      )
+      .then((response) => {
+        context.commit('setAvailableDevices', response.data.results)
+      })
+      .catch((error) => {
+        context.dispatch('ui/notifyError', error, { root: true })
+      })
 }
 
 export async function getFeaturesDevices(context) {
@@ -37,22 +39,23 @@ export async function getFeaturesDevices(context) {
 }
 
 export async function getLogicalDevice(context, { id, index }) {
-  await this.$axios
-    .get(
-      `${context.rootState.app.initialUrl.token}${tokenApi.availableLogicalDevices}${context.rootState.computer.cid}&did=${id}`,
-      { headers: { Authorization: context.rootState.app.tokenValue } }
-    )
-    .then((response) => {
-      if (response.data.results) {
-        let payload = {}
-        payload.results = response.data.results
-        payload.index = index
-        context.commit('addLogicalDevices', payload)
-      }
-    })
-    .catch((error) => {
-      context.dispatch('ui/notifyError', error, { root: true })
-    })
+  if (context.rootState.computer.cid)
+    await this.$axios
+      .get(
+        `${context.rootState.app.initialUrl.token}${tokenApi.availableLogicalDevices}${context.rootState.computer.cid}&did=${id}`,
+        { headers: { Authorization: context.rootState.app.tokenValue } }
+      )
+      .then((response) => {
+        if (response.data.results) {
+          let payload = {}
+          payload.results = response.data.results
+          payload.index = index
+          context.commit('addLogicalDevices', payload)
+        }
+      })
+      .catch((error) => {
+        context.dispatch('ui/notifyError', error, { root: true })
+      })
 }
 
 export function changeDeviceAttributes(
