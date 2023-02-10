dadc6dfa8a32dacbaf13fed1b102d8c30289f949
jact <jachavar@gmail.com>
2023-02-10 08:57:16 +0100

    Fixed notifyError function

--- src/store/ui/actions.js
+++ src/store/ui/actions.js
@@ -14,13 +14,27 @@ export function notifyError(context, error) {
   let message = ''
 
   // Setup Error Message
-  if (typeof error !== 'undefined') {
+  if (typeof error !== undefined) {
     if (error.hasOwnProperty('message')) {
       message = error.message
     }
   }
 
-  if (typeof error.response !== 'undefined') {
+  if (
+    (error.hasOwnProperty('code') && error.code === 'ERR_NETWORK') ||
+    !error.response
+  ) {
+    context.commit(
+      'app/setStatus',
+      this.$gettext.$gettext('There is no connection to the server'),
+      { root: true }
+    )
+    context.commit('app/stopApp', null, { root: true })
+
+    return
+  }
+
+  if (typeof error.response !== undefined) {
     // Setup Generic Response Messages
     if (error.response.status === 401) {
       message = 'UnAuthorized'
@@ -38,7 +52,8 @@ export function notifyError(context, error) {
     // Try to Use the Response Message
     if (
       error.hasOwnProperty('response') &&
-      error.response.hasOwnProperty('data')
+      error.response.hasOwnProperty('data') &&
+      error.response.data !== undefined
     ) {
       if (Object.keys(error.response.data).length > 0) {
         message = error.response.data[Object.keys(error.response.data)[0]]
