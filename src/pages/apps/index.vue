eb8a62b7f624e2dd2adb272856def6175dfccd4c
jact <jachavar@gmail.com>
2023-02-15 20:30:57 +0100

    Changed vuex by pinia

--- src/pages/apps/index.vue
+++ src/pages/apps/index.vue
@@ -7,15 +7,15 @@
         size="sm"
         flat
         color="primary"
-        :loading="$store.state.ui.isUpdating"
-        :disabled="$store.state.ui.isUpdating"
+        :loading="isUpdating"
+        :disabled="isUpdating"
         @click="updateApps"
       >
         <q-tooltip>{{ $gettext('Update') }}</q-tooltip></q-btn
       >
     </q-breadcrumbs>
 
-    <div v-if="$store.state.ui.isUpdating" class="row q-ma-xl">
+    <div v-if="isUpdating" class="row q-ma-xl">
       <div class="col-12 text-center">
         <q-spinner color="primary" size="6em" />
       </div>
@@ -26,30 +26,39 @@
 </template>
 
 <script>
-import { useStore } from 'vuex'
+import { storeToRefs } from 'pinia'
 import { useGettext } from 'vue3-gettext'
 import { useMeta } from 'quasar'
 
 import Apps from 'components/Apps.vue'
+
+import { useAppStore } from 'src/stores/app'
+import { useFiltersStore } from 'src/stores/filters'
+import { useUiStore } from 'src/stores/ui'
 
 export default {
   components: {
     Apps,
   },
   setup() {
-    const store = useStore()
     const { $gettext } = useGettext()
+
+    const appStore = useAppStore()
+    const filtersStore = useFiltersStore()
+    const uiStore = useUiStore()
+
+    const { isUpdating } = storeToRefs(uiStore)
 
     useMeta({ title: $gettext('Apps') })
 
     const updateApps = async () => {
-      store.commit('ui/updating')
-      await store.dispatch('app/getApps')
-      await store.dispatch('filters/setCategories')
-      store.commit('ui/updatingFinished')
+      uiStore.updating()
+      await appStore.loadApps()
+      await filtersStore.setCategories()
+      uiStore.updatingFinished()
     }
 
-    return { updateApps }
+    return { isUpdating, updateApps }
   },
 }
 </script>
