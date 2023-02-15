69f0e424d0614407435df23b6a67cbcbf1ad6095
jact <jachavar@gmail.com>
2023-02-15 20:31:14 +0100

    Changed vuex by pinia

--- src/pages/devices/index.vue
+++ src/pages/devices/index.vue
@@ -7,15 +7,15 @@
         size="sm"
         flat
         color="primary"
-        :loading="$store.state.ui.isUpdating"
-        :disabled="$store.state.ui.isUpdating"
+        :loading="isUpdating"
+        :disabled="isUpdating"
         @click="updateDevices"
       >
         <q-tooltip>{{ $gettext('Update') }}</q-tooltip></q-btn
       >
     </q-breadcrumbs>
 
-    <div v-if="$store.state.ui.isUpdating" class="row q-ma-xl">
+    <div v-if="isUpdating" class="row q-ma-xl">
       <div class="col-12 text-center">
         <q-spinner color="primary" size="6em" />
       </div>
@@ -26,31 +26,38 @@
 </template>
 
 <script>
-import { useStore } from 'vuex'
+import { storeToRefs } from 'pinia'
 import { useGettext } from 'vue3-gettext'
 import { useMeta } from 'quasar'
 
 import Devices from 'components/Devices.vue'
+
+import { useDevicesStore } from 'src/stores/devices'
+import { useUiStore } from 'src/stores/ui'
 
 export default {
   components: {
     Devices,
   },
   setup() {
-    const store = useStore()
     const { $gettext } = useGettext()
+
+    const devicesStore = useDevicesStore()
+    const uiStore = useUiStore()
+
+    const { isUpdating } = storeToRefs(uiStore)
 
     useMeta({ title: $gettext('Devices') })
 
     const updateDevices = async () => {
-      store.commit('ui/updating')
-      await store.dispatch('devices/computerDevices')
-      await store.dispatch('devices/getAvailableDevices')
-      await store.dispatch('devices/getFeaturesDevices')
-      store.commit('ui/updatingFinished')
+      uiStore.updating()
+      await devicesStore.computerDevices()
+      await devicesStore.getAvailableDevices()
+      await devicesStore.getFeaturesDevices()
+      uiStore.updatingFinished()
     }
 
-    return { updateDevices }
+    return { isUpdating, updateDevices }
   },
 }
 </script>
