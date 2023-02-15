7ca803daca62640bbed14fed5d41dd1aa1adea9f
jact <jachavar@gmail.com>
2023-02-15 20:31:29 +0100

    Changed vuex by pinia

--- src/pages/info/index.vue
+++ src/pages/info/index.vue
@@ -7,15 +7,15 @@
         size="sm"
         flat
         color="primary"
-        :loading="$store.state.ui.isUpdating"
-        :disabled="$store.state.ui.isUpdating"
+        :loading="isUpdating"
+        :disabled="isUpdating"
         @click="updateInfo"
       >
         <q-tooltip>{{ $gettext('Update') }}</q-tooltip></q-btn
       >
     </q-breadcrumbs>
 
-    <div v-if="$store.state.ui.isUpdating" class="row q-ma-xl">
+    <div v-if="isUpdating" class="row q-ma-xl">
       <div class="col-12 text-center">
         <q-spinner color="primary" size="6em" />
       </div>
@@ -26,33 +26,40 @@
 </template>
 
 <script>
-import { useStore } from 'vuex'
+import { storeToRefs } from 'pinia'
 import { useGettext } from 'vue3-gettext'
 import { useMeta } from 'quasar'
 
 import Info from 'components/Info.vue'
+
+import { useComputerStore } from 'src/stores/computer'
+import { useUiStore } from 'src/stores/ui'
 
 export default {
   components: {
     Info,
   },
   setup() {
-    const store = useStore()
     const { $gettext } = useGettext()
+
+    const computerStore = useComputerStore()
+    const uiStore = useUiStore()
+
+    const { isUpdating } = storeToRefs(uiStore)
 
     useMeta({ title: $gettext('Info') })
 
     const updateInfo = async () => {
-      store.commit('ui/updating')
-      await store.dispatch('computer/computerInfo')
-      await store.dispatch('computer/computerNetwork')
-      await store.dispatch('computer/computerId')
-      await store.dispatch('computer/computerData')
-      await store.dispatch('computer/computerAttribute')
-      store.commit('ui/updatingFinished')
+      uiStore.updating()
+      await computerStore.computerInfo()
+      await computerStore.computerNetwork()
+      await computerStore.computerId()
+      await computerStore.computerData()
+      await computerStore.computerAttribute()
+      uiStore.updatingFinished()
     }
 
-    return { updateInfo }
+    return { isUpdating, updateInfo }
   },
 }
 </script>
