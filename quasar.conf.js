5b10d291e52c9e62def98b897fb2c73d138739de
jact <jachavar@gmail.com>
2023-02-15 21:28:15 +0100

    Removed pinia from boot files

--- quasar.conf.js
+++ quasar.conf.js
@@ -23,7 +23,7 @@ module.exports = configure(function (ctx) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli/boot-files
-    boot: ['axios', 'fonts', 'gettext', 'pinia'],
+    boot: ['axios', 'fonts', 'gettext'],
 
     // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: ['app.scss'],
