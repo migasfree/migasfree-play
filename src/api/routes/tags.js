244e030dbbf8245eea787cea048f1965e20c8223
jact <jachavar@gmail.com>
2023-02-24 13:31:17 +0100

    feat(v4): compatibility with v4 in /available and /assigned routes

--- src/api/routes/tags.js
+++ src/api/routes/tags.js
@@ -6,11 +6,20 @@ const router = express.Router()
 const options = { env: { MIGASFREE_CLIENT_DEBUG: 0 } }
 
 router.get('/available', (req, res) => {
-  const code = `
+  let code = `
 import json
 from migasfree_client.tags import MigasFreeTags
 
 print(json.dumps(MigasFreeTags().get_available_tags()))`
+
+  if (req.query.version.startsWith('4.'))
+    code = `
+import json
+from migasfree_client.tags import MigasFreeTags
+
+mft = MigasFreeTags()
+response = mft._get_tags()
+print(json.dumps(response['available'], ensure_ascii=False))`
 
   PythonShell.runString(code, options, (err, results) => {
     if (err) throw err
@@ -20,11 +29,20 @@ print(json.dumps(MigasFreeTags().get_available_tags()))`
 })
 
 router.get('/assigned', (req, res) => {
-  const code = `
+  let code = `
 import json
 from migasfree_client.tags import MigasFreeTags
 
 print(json.dumps(MigasFreeTags().get_assigned_tags()))`
+
+  if (req.query.version.startsWith('4.'))
+    code = `
+import json
+from migasfree_client.tags import MigasFreeTags
+
+mft = MigasFreeTags()
+response = mft._get_tags()
+print(json.dumps(response['selected'], ensure_ascii=False))`
 
   PythonShell.runString(code, options, (err, results) => {
     if (err) throw err
