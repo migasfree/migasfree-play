6126e1fe23e60dea23c9bbfd38a3e698ab5c23c6
Jose Antonio Chavarría <jachavar@gmail.com>
2023-02-28 14:39:08 +0100

    refactor: usage of filterDevices action

--- src/components/DeviceFilter.vue
+++ src/components/DeviceFilter.vue
@@ -8,6 +8,7 @@
               v-model="filtersStore.searchDevice"
               :placeholder="$gettext('Search in name or model or manufacturer')"
               clearable
+              @update:model-value="devicesStore.filterDevices"
               ><template #prepend><q-icon name="mdi-magnify" /></template
             ></q-input>
           </p>
@@ -22,6 +23,7 @@
               "
               :false-value="false"
               :true-value="true"
+              @update:model-value="devicesStore.filterDevices"
             />
           </p>
         </q-card-section>
@@ -31,14 +33,16 @@
 </template>
 
 <script>
+import { useDevicesStore } from 'src/stores/devices'
 import { useFiltersStore } from 'src/stores/filters'
 
 export default {
   name: 'DeviceFilter',
   setup() {
+    const devicesStore = useDevicesStore()
     const filtersStore = useFiltersStore()
 
-    return { filtersStore }
+    return { devicesStore, filtersStore }
   },
 }
 </script>
