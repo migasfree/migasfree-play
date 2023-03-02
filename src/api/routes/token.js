91d87ed1e055a30732861ebc04c62a097b61d363
Jose Antonio Chavarría <jachavar@gmail.com>
2023-03-02 13:35:22 +0100

    feat(debug): usage of debug setting

--- src/api/routes/token.js
+++ src/api/routes/token.js
@@ -2,11 +2,14 @@ const os = require('os')
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
+const { debug } = require('../utils')
 
 const tokenFile = path.join(os.homedir(), '.migasfree-play', 'token')
 const router = express.Router()
 
 router.get('/', (req, res) => {
+  if (debug) console.log('[express] Reading token file...')
+
   let token = ''
 
   if (fs.existsSync(tokenFile)) {
@@ -16,7 +19,11 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body)
+  if (debug) {
+    console.log('[express] Writing token file...')
+    console.log(req.body)
+  }
+
   fs.mkdirSync(path.dirname(tokenFile), { recursive: true })
   fs.writeFileSync(tokenFile, req.body.token)
   res.send()
