210b4253f83610f53650cca112959d979996dfc7
Jose Antonio Chavarría <jachavar@gmail.com>
2023-03-02 13:35:30 +0100

    feat(debug): usage of debug setting

--- src/api/routes/executions.js
+++ src/api/routes/executions.js
@@ -2,11 +2,14 @@ const os = require('os')
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
+const { debug } = require('../utils')
 
 const filePath = path.join(os.homedir(), '.migasfree-play', 'executions.json')
 const router = express.Router()
 
 router.get('/', (req, res) => {
+  if (debug) console.log('[express] Reading executions file...')
+
   if (fs.existsSync(filePath)) {
     const data = fs.readFileSync(filePath, 'utf8')
 
@@ -18,6 +21,8 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
+  if (debug) console.log('[express] Writing executions file...')
+
   fs.mkdirSync(path.dirname(filePath), { recursive: true })
   fs.writeFileSync(filePath, JSON.stringify(req.body, null, 2))
   res.send()
