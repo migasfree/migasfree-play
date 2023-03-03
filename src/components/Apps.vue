3656b92ffd56a28697390968c9cafbd89a91e42d
jact <jachavar@gmail.com>
2023-03-03 13:50:56 +0100

    feat(pagination): usage of Pagination component

--- src/components/Apps.vue
+++ src/components/Apps.vue
@@ -1,20 +1,28 @@
 <template>
   <AppFilter />
 
-  <div v-if="filteredApps.length > 0" class="row">
-    <AppDetail
-      v-for="item in filteredApps"
-      :key="item.id"
-      :icon="item.icon || ''"
-      :name="item.name"
-      :category="item.category.name"
-      :score="item.score"
-      :description="item.description"
-      :packages="item.packages_to_install"
-      :level="item.level.id"
-      @openlogin="openLogin"
+  <template v-if="filteredApps.length > 0">
+    <div class="row">
+      <AppDetail
+        v-for="item in paginatedApps"
+        :key="item.id"
+        :icon="item.icon || ''"
+        :name="item.name"
+        :category="item.category.name"
+        :score="item.score"
+        :description="item.description"
+        :packages="item.packages_to_install"
+        :level="item.level.id"
+        @openlogin="openLogin"
+      />
+    </div>
+
+    <Pagination
+      class="justify-center"
+      :total="filteredApps.length"
+      :page-changed="pageChanged"
     />
-  </div>
+  </template>
   <q-banner v-else class="bg-info text-black q-ma-md">
     <template #avatar>
       <q-icon name="mdi-information-outline" color="white" />
@@ -26,14 +34,18 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { storeToRefs } from 'pinia'
 
-import AppFilter from 'components/AppFilter.vue'
-import AppDetail from 'components/AppDetail.vue'
+import AppFilter from 'components/AppFilter'
+import AppDetail from 'components/AppDetail'
 import Login from 'components/Login'
+import Pagination from 'components/Pagination'
 
 import { useAppStore } from 'src/stores/app'
+import { useUiStore } from 'src/stores/ui'
+
+import { resultsPerPage } from 'config/app.conf'
 
 export default {
   name: 'Apps',
@@ -41,18 +53,36 @@ export default {
     AppFilter,
     AppDetail,
     Login,
+    Pagination,
   },
   setup() {
     const appStore = useAppStore()
+    const uiStore = useUiStore()
 
     const { filteredApps } = storeToRefs(appStore)
     const showLogin = ref(false)
+    const paginatedApps = ref(filteredApps.value.slice(0, resultsPerPage))
 
     const openLogin = () => {
       showLogin.value = true
     }
 
-    return { filteredApps, showLogin, openLogin }
+    const pageChanged = (currentPage = 1) => {
+      const start = (currentPage - 1) * resultsPerPage
+      const end = start + resultsPerPage
+
+      paginatedApps.value = filteredApps.value.slice(start, end)
+
+      setTimeout(() => {
+        uiStore.scrollToElement(document.getElementById('main'))
+      }, 250)
+    }
+
+    watch(filteredApps, () => {
+      pageChanged()
+    })
+
+    return { filteredApps, paginatedApps, showLogin, openLogin, pageChanged }
   },
 }
 </script>
