a7dc3322e11216315e555290db8bc915954b3d33
jact <jachavar@gmail.com>
2023-03-03 13:58:53 +0100

    feat(pagination): usage of Pagination component

--- src/components/Devices.vue
+++ src/components/Devices.vue
@@ -1,19 +1,27 @@
 <template>
   <DeviceFilter />
 
-  <div v-if="filteredDevices.length > 0" class="row">
-    <DeviceDetail
-      v-for="item in filteredDevices"
-      :id="item.name"
-      :key="item.id"
-      :icon="icon(item.connection.name)"
-      :name="name(item)"
-      :connection="item.connection.name"
-      :description="description(item.data)"
-      :ip="ipAddress(item.data)"
-      :logical="item.logical"
+  <template v-if="filteredDevices.length > 0">
+    <div class="row">
+      <DeviceDetail
+        v-for="item in filteredDevices"
+        :id="item.name"
+        :key="item.id"
+        :icon="icon(item.connection.name)"
+        :name="name(item)"
+        :connection="item.connection.name"
+        :description="description(item.data)"
+        :ip="ipAddress(item.data)"
+        :logical="item.logical"
+      />
+    </div>
+
+    <Pagination
+      class="justify-center"
+      :total="filteredDevices.length"
+      :page-changed="pageChanged"
     />
-  </div>
+  </template>
   <q-banner v-else class="bg-info text-black q-ma-md">
     <template #avatar>
       <q-icon name="mdi-information-outline" color="white" />
@@ -23,22 +31,31 @@
 </template>
 
 <script>
+import { ref, watch } from 'vue'
 import { storeToRefs } from 'pinia'
 
-import DeviceFilter from 'components/DeviceFilter.vue'
-import DeviceDetail from 'components/DeviceDetail.vue'
+import DeviceFilter from 'components/DeviceFilter'
+import DeviceDetail from 'components/DeviceDetail'
+import Pagination from 'components/Pagination'
 
 import { useDevicesStore } from 'src/stores/devices'
+import { useUiStore } from 'src/stores/ui'
+
+import { resultsPerPage } from 'config/app.conf'
 
 export default {
   name: 'Devices',
   components: {
     DeviceFilter,
     DeviceDetail,
+    Pagination,
   },
   setup() {
     const devicesStore = useDevicesStore()
+    const uiStore = useUiStore()
+
     const { filteredDevices } = storeToRefs(devicesStore)
+    const paginatedDevices = ref(filteredDevices.value.slice(0, resultsPerPage))
 
     const name = (item) => {
       return 'NAME' in item.data && item.data.NAME
@@ -60,7 +77,30 @@ export default {
       return value.LOCATION || ''
     }
 
-    return { filteredDevices, name, icon, ipAddress, description }
+    const pageChanged = (currentPage = 1) => {
+      const start = (currentPage - 1) * resultsPerPage
+      const end = start + resultsPerPage
+
+      paginatedDevices.value = filteredDevices.value.slice(start, end)
+
+      setTimeout(() => {
+        uiStore.scrollToElement(document.getElementById('main'))
+      }, 250)
+    }
+
+    watch(filteredDevices, () => {
+      pageChanged()
+    })
+
+    return {
+      filteredDevices,
+      paginatedDevices,
+      name,
+      icon,
+      ipAddress,
+      description,
+      pageChanged,
+    }
   },
 }
 </script>
