01f382325c064aad31a7e3fb7ddb4b9d810e3f5c
Jose Antonio Chavarría <jachavar@gmail.com>
2023-03-08 13:24:13 +0100

    refactor: setup store syntax

--- src/stores/executions.js
+++ src/stores/executions.js
@@ -1,3 +1,4 @@
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import { date } from 'quasar'
 
@@ -11,206 +12,201 @@ import { internalApi, executionsMaxLength } from 'config/app.conf'
 
 const app = window.electronRemote.app // electron-preload.js
 
-export const useExecutionsStore = defineStore('executions', {
-  state: () => ({
-    log: {},
-    lastId: '',
-    isRunningCommand: false,
-    error: '',
-  }),
-  actions: {
-    escapeRegExp(text) {
-      return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
-    },
+export const useExecutionsStore = defineStore('executions', () => {
+  const items = ref({})
+  const lastId = ref('')
+  const isRunningCommand = ref(false)
+  const error = ref('')
 
-    replaceAll(str, find, replace) {
-      var exp = this.escapeRegExp(find)
-      var re = new RegExp(exp, 'g')
+  function escapeRegExp(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
+  }
 
-      return str.replace(re, replace)
-    },
+  function replaceAll(str, find, replace) {
+    var exp = escapeRegExp(find)
+    var re = new RegExp(exp, 'g')
 
-    replaceColors(txt) {
-      txt = txt.replace(/\\x1b\[\?25l([\s\S]*?)\\x1b\[\?25h/g, '')
+    return str.replace(re, replace)
+  }
 
-      txt = this.replaceAll(txt, '\r\x1b[2K\x1b[32m⠋\x1b[0m', '')
-      txt = this.replaceAll(txt, '\r\x1b[2K\x1b[32m⠙\x1b[0m', '')
-      txt = this.replaceAll(txt, '\r\x1b[2K\x1b[32m⠹\x1b[0m', '')
-      txt = this.replaceAll(txt, '\r\x1b[2K\x1b[32m⠸\x1b[0m', '')
-      txt = this.replaceAll(txt, '\r\x1b[2K\x1b[32m⠼\x1b[0m', '')
-      txt = this.replaceAll(txt, '\r\x1b[2K\x1b[32m⠴\x1b[0m', '')
-      txt = this.replaceAll(txt, '\r\x1b[2K\x1b[32m⠦\x1b[0m', '')
-      txt = this.replaceAll(txt, '\r\x1b[2K\x1b[32m⠧\x1b[0m', '')
-      txt = this.replaceAll(txt, '\r\x1b[2K\x1b[32m⠇\x1b[0m', '')
-      txt = this.replaceAll(txt, '\r\x1b[2K\x1b[32m⠏\x1b[0m', '')
+  function replaceColors(txt) {
+    txt = txt.replace(/\\x1b\[\?25l([\s\S]*?)\\x1b\[\?25h/g, '')
 
-      txt = this.replaceAll(txt, '\x1b[32m⠋\x1b[0m', '')
-      txt = this.replaceAll(txt, '\x1b[32m⠙\x1b[0m', '')
-      txt = this.replaceAll(txt, '\x1b[32m⠹\x1b[0m', '')
-      txt = this.replaceAll(txt, '\x1b[32m⠸\x1b[0m', '')
-      txt = this.replaceAll(txt, '\x1b[32m⠼\x1b[0m', '')
-      txt = this.replaceAll(txt, '\x1b[32m⠴\x1b[0m', '')
-      txt = this.replaceAll(txt, '\x1b[32m⠦\x1b[0m', '')
-      txt = this.replaceAll(txt, '\x1b[32m⠧\x1b[0m', '')
-      txt = this.replaceAll(txt, '\x1b[32m⠇\x1b[0m', '')
-      txt = this.replaceAll(txt, '\x1b[32m⠏\x1b[0m', '')
+    txt = replaceAll(txt, '\r\x1b[2K\x1b[32m⠋\x1b[0m', '')
+    txt = replaceAll(txt, '\r\x1b[2K\x1b[32m⠙\x1b[0m', '')
+    txt = replaceAll(txt, '\r\x1b[2K\x1b[32m⠹\x1b[0m', '')
+    txt = replaceAll(txt, '\r\x1b[2K\x1b[32m⠸\x1b[0m', '')
+    txt = replaceAll(txt, '\r\x1b[2K\x1b[32m⠼\x1b[0m', '')
+    txt = replaceAll(txt, '\r\x1b[2K\x1b[32m⠴\x1b[0m', '')
+    txt = replaceAll(txt, '\r\x1b[2K\x1b[32m⠦\x1b[0m', '')
+    txt = replaceAll(txt, '\r\x1b[2K\x1b[32m⠧\x1b[0m', '')
+    txt = replaceAll(txt, '\r\x1b[2K\x1b[32m⠇\x1b[0m', '')
+    txt = replaceAll(txt, '\r\x1b[2K\x1b[32m⠏\x1b[0m', '')
 
-      txt = this.replaceAll(txt, '\x1b[95m', "<span class='text-purple'>") // accent
-      txt = this.replaceAll(txt, '\x1b[35m', "<span class='text-purple'>") // accent
-      txt = this.replaceAll(txt, '\x1b[92m', "<span class='text-green'>") // ok
-      txt = this.replaceAll(txt, '\x1b[1;32m', "<span class='text-green'>") // ok
-      txt = this.replaceAll(
-        txt,
-        '\x1b[1;92m',
-        "<span class='text-light-green'>"
-      ) // ok
-      txt = this.replaceAll(txt, '\x1b[93m', "<span class='text-amber'>") // warning
-      txt = this.replaceAll(txt, '\x1b[91m', "<span class='text-negative'>") // error
-      txt = this.replaceAll(txt, '\x1b[1;91m', "<span class='text-negative'>") // error
-      txt = this.replaceAll(txt, '\x1b[33m', "<span class='text-amber'>") // warning
-      txt = this.replaceAll(txt, '\x1b[32m', "<span class='text-blue'>") // info
-      txt = this.replaceAll(txt, '\x1b[1;34m', "<span class='text-blue'>") // info
-      txt = this.replaceAll(txt, '\x1b[1;36m', "<span class='text-indigo'>") // info
-      txt = this.replaceAll(txt, '\x1b[2;36m', "<span class='text-teal'>") // time
-      txt = this.replaceAll(txt, '\x1b[4;94m', "<span class='text-blue'>") // info
-      txt = this.replaceAll(txt, '\x1b[0m', '</span>')
-      txt = this.replaceAll(txt, '\r\x1b[2K', '')
-      txt = this.replaceAll(txt, '\x1b[2K', '')
-      txt = this.replaceAll(txt, '\x1b[?25l', '')
-      txt = this.replaceAll(txt, '\x1b[?25h', '')
-      txt = this.replaceAll(txt, '\x1b[1A', '')
-      txt = this.replaceAll(txt, '\x1b[1m', '')
-      txt = txt.replace(/(?:\r\n|\r|\n)/g, '<br />')
+    txt = replaceAll(txt, '\x1b[32m⠋\x1b[0m', '')
+    txt = replaceAll(txt, '\x1b[32m⠙\x1b[0m', '')
+    txt = replaceAll(txt, '\x1b[32m⠹\x1b[0m', '')
+    txt = replaceAll(txt, '\x1b[32m⠸\x1b[0m', '')
+    txt = replaceAll(txt, '\x1b[32m⠼\x1b[0m', '')
+    txt = replaceAll(txt, '\x1b[32m⠴\x1b[0m', '')
+    txt = replaceAll(txt, '\x1b[32m⠦\x1b[0m', '')
+    txt = replaceAll(txt, '\x1b[32m⠧\x1b[0m', '')
+    txt = replaceAll(txt, '\x1b[32m⠇\x1b[0m', '')
+    txt = replaceAll(txt, '\x1b[32m⠏\x1b[0m', '')
 
-      return txt
-    },
+    txt = replaceAll(txt, '\x1b[95m', "<span class='text-purple'>") // accent
+    txt = replaceAll(txt, '\x1b[35m', "<span class='text-purple'>") // accent
+    txt = replaceAll(txt, '\x1b[92m', "<span class='text-green'>") // ok
+    txt = replaceAll(txt, '\x1b[1;32m', "<span class='text-green'>") // ok
+    txt = replaceAll(txt, '\x1b[1;92m', "<span class='text-light-green'>") // ok
+    txt = replaceAll(txt, '\x1b[93m', "<span class='text-amber'>") // warning
+    txt = replaceAll(txt, '\x1b[91m', "<span class='text-negative'>") // error
+    txt = replaceAll(txt, '\x1b[1;91m', "<span class='text-negative'>") // error
+    txt = replaceAll(txt, '\x1b[33m', "<span class='text-amber'>") // warning
+    txt = replaceAll(txt, '\x1b[32m', "<span class='text-blue'>") // info
+    txt = replaceAll(txt, '\x1b[1;34m', "<span class='text-blue'>") // info
+    txt = replaceAll(txt, '\x1b[1;36m', "<span class='text-indigo'>") // info
+    txt = replaceAll(txt, '\x1b[2;36m', "<span class='text-teal'>") // time
+    txt = replaceAll(txt, '\x1b[4;94m', "<span class='text-blue'>") // info
+    txt = replaceAll(txt, '\x1b[0m', '</span>')
+    txt = replaceAll(txt, '\r\x1b[2K', '')
+    txt = replaceAll(txt, '\x1b[2K', '')
+    txt = replaceAll(txt, '\x1b[?25l', '')
+    txt = replaceAll(txt, '\x1b[?25h', '')
+    txt = replaceAll(txt, '\x1b[1A', '')
+    txt = replaceAll(txt, '\x1b[1m', '')
+    txt = txt.replace(/(?:\r\n|\r|\n)/g, '<br />')
 
-    async getExecutions() {
-      const uiStore = useUiStore()
+    return txt
+  }
 
-      await api
-        .get(`${internalApi}/executions`)
-        .then((response) => {
-          this.setExecutionsLog(response.data)
-        })
-        .catch((error) => {
-          uiStore.notifyError(error)
-        })
-    },
+  async function getExecutions() {
+    const uiStore = useUiStore()
 
-    async setExecutions() {
-      const uiStore = useUiStore()
-
-      await api.post(`${internalApi}/executions`, this.log).catch((error) => {
+    await api
+      .get(`${internalApi}/executions`)
+      .then((response) => {
+        setExecutionsLog(response.data)
+      })
+      .catch((error) => {
         uiStore.notifyError(error)
       })
-    },
+  }
 
-    run({ cmd, text, icon }) {
-      const packagesStore = usePackagesStore()
-      const uiStore = useUiStore()
+  async function setExecutions() {
+    const uiStore = useUiStore()
 
-      if (this.isRunningCommand) {
-        uiStore.notifyInfo(
-          gettext.$gettext('Please wait, other process is running!!!')
-        )
-        return
-      }
-      this.startedCmd()
+    await api.post(`${internalApi}/executions`, items.value).catch((error) => {
+      uiStore.notifyError(error)
+    })
+  }
 
-      const spawn = require('child_process').spawn
-      let subprocess
+  function run({ cmd, text, icon }) {
+    const packagesStore = usePackagesStore()
+    const uiStore = useUiStore()
 
-      const [command, ...args] = cmd.split(' ')
-
-      if (process.platform === 'linux') {
-        subprocess = spawn(command, args, { shell: '/bin/bash' })
-      } else if (process.platform === 'win32') {
-        subprocess = spawn(command, args, { shell: true })
-      }
-
-      this.addExecution({ command: text, icon })
-
-      subprocess.stdout.on('data', (data) => {
-        this.appendExecutionText(this.replaceColors(data.toString()))
-      })
-
-      subprocess.stderr.on('data', (data) => {
-        const text = this.replaceColors(data.toString())
-
-        this.appendExecutionError(text)
-        this.appendExecutionText(text)
-      })
-
-      // when the spawn child process exits, check if there were any errors
-      subprocess.on('exit', (code) => {
-        const win = window.electronRemote.getCurrentWindow() // electron-preload.js
-
-        if (code !== 0) {
-          uiStore.notifyError(`Error: ${code} ${cmd}`)
-          win.show()
-        } else {
-          if (this.error === '') {
-            packagesStore.setInstalledPackages()
-          } else {
-            uiStore.notifyError(
-              this.replaceColors(this.error).replace(/(<([^>]+)>)/gi, '')
-            )
-            this.resetExecutionError()
-          }
-        }
-
-        this.setExecutions()
-        this.finishedCmd()
-
-        if (cmd.includes('sync')) {
-          if (win.isMinimized()) win.close()
-
-          packagesStore.setAvailablePackages()
-        }
-      })
-    },
-
-    setExecutionsLog(value) {
-      this.log = value
-      if (Object.keys(value).length)
-        this.lastId = Object.keys(value)[Object.keys(value).length - 1]
-    },
-
-    async startedCmd() {
-      this.isRunningCommand = true
-      app.canExit = false
-    },
-
-    async finishedCmd() {
-      this.isRunningCommand = false
-      app.canExit = true
-    },
-
-    addExecution({ command, icon }) {
-      this.lastId = date.formatDate(
-        Date.parse(new Date()),
-        'YYYY-MM-DD HH:mm:ss'
+    if (isRunningCommand.value) {
+      uiStore.notifyInfo(
+        gettext.$gettext('Please wait, other process is running!!!')
       )
-      this.log[this.lastId] = {
-        command,
-        icon,
-        text: '',
+      return
+    }
+    startedCmd()
+
+    const spawn = require('child_process').spawn
+    let subprocess
+
+    const [command, ...args] = cmd.split(' ')
+
+    if (process.platform === 'linux') {
+      subprocess = spawn(command, args, { shell: '/bin/bash' })
+    } else if (process.platform === 'win32') {
+      subprocess = spawn(command, args, { shell: true })
+    }
+
+    addExecution({ command: text, icon })
+
+    subprocess.stdout.on('data', (data) => {
+      appendExecutionText(replaceColors(data.toString()))
+    })
+
+    subprocess.stderr.on('data', (data) => {
+      const text = replaceColors(data.toString())
+
+      appendExecutionError(text)
+      appendExecutionText(text)
+    })
+
+    // when the spawn child process exits, check if there were any errors
+    subprocess.on('exit', (code) => {
+      const win = window.electronRemote.getCurrentWindow() // electron-preload.js
+
+      if (code !== 0) {
+        uiStore.notifyError(`Error: ${code} ${cmd}`)
+        win.show()
+      } else {
+        if (error.value === '') {
+          packagesStore.setInstalledPackages()
+        } else {
+          uiStore.notifyError(
+            replaceColors(error.value).replace(/(<([^>]+)>)/gi, '')
+          )
+          resetExecutionError()
+        }
       }
-      while (Object.keys(this.log).length > executionsMaxLength)
-        delete this.log[Object.keys(this.log)[0]]
-    },
 
-    appendExecutionText(text) {
-      this.log[this.lastId]['text'] += text
-    },
+      setExecutions()
+      finishedCmd()
 
-    appendExecutionError(text) {
-      this.error += text
-    },
+      if (cmd.includes('sync')) {
+        if (win.isMinimized()) win.close()
 
-    resetExecutionError() {
-      this.error = ''
-    },
-  },
+        packagesStore.setAvailablePackages()
+      }
+    })
+  }
+
+  function setExecutionsLog(value) {
+    items.value = value
+    if (Object.keys(value).length)
+      lastId.value = Object.keys(value)[Object.keys(value).length - 1]
+  }
+
+  async function startedCmd() {
+    isRunningCommand.value = true
+    app.canExit = false
+  }
+
+  async function finishedCmd() {
+    isRunningCommand.value = false
+    app.canExit = true
+  }
+
+  function addExecution({ command, icon }) {
+    lastId.value = date.formatDate(
+      Date.parse(new Date()),
+      'YYYY-MM-DD HH:mm:ss'
+    )
+    items.value[lastId.value] = {
+      command,
+      icon,
+      text: '',
+    }
+    while (Object.keys(items.value).length > executionsMaxLength)
+      delete items.value[Object.keys(items.value)[0]]
+  }
+
+  function appendExecutionText(text) {
+    items.value[lastId.value]['text'] += text
+  }
+
+  function appendExecutionError(text) {
+    error.value += text
+  }
+
+  function resetExecutionError() {
+    error.value = ''
+  }
+
+  return { items, lastId, isRunningCommand, error, getExecutions, run }
 })
