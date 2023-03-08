e8a75457dba7bec461b7583c8491dab687b5f6e1
Jose Antonio Chavarría <jachavar@gmail.com>
2023-03-08 13:24:55 +0100

    feat(count): added count

--- src/pages/details/index.vue
+++ src/pages/details/index.vue
@@ -2,6 +2,17 @@
   <q-page id="main" padding>
     <q-breadcrumbs>
       <q-breadcrumbs-el :label="$gettext('Details')" icon="mdi-list-status" />
+
+      <q-chip
+        v-if="count > 0"
+        outline
+        color="primary"
+        text-color="white"
+        class="text-right"
+        size="sm"
+      >
+        {{ count }}
+      </q-chip>
     </q-breadcrumbs>
 
     <Executions />
@@ -11,8 +22,11 @@
 <script>
 import { useGettext } from 'vue3-gettext'
 import { useMeta } from 'quasar'
+import { storeToRefs } from 'pinia'
 
 import Executions from 'components/Executions.vue'
+
+import { useExecutionsStore } from 'src/stores/executions'
 
 export default {
   components: {
@@ -20,8 +34,12 @@ export default {
   },
   setup() {
     const { $gettext } = useGettext()
+    const executionsStore = useExecutionsStore()
+    const { items } = storeToRefs(executionsStore)
 
     useMeta({ title: $gettext('Details') })
+
+    return { count: Object.keys(items.value).length }
   },
 }
 </script>
