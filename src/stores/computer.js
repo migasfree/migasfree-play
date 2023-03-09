f05ef7a3707caa77ddfae14e5808c476c8d4e8ca
Jose Antonio Chavarría <jachavar@gmail.com>
2023-03-09 12:42:12 +0100

    refactor: setup store syntax

--- src/stores/computer.js
+++ src/stores/computer.js
@@ -1,4 +1,5 @@
-import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { defineStore, storeToRefs } from 'pinia'
 
 import { api } from 'boot/axios'
 
@@ -7,127 +8,169 @@ import { useUiStore } from './ui'
 
 import { internalApi, tokenApi } from 'config/app.conf'
 
-export const useComputerStore = defineStore('computer', {
-  state: () => ({
-    name: '',
-    uuid: '',
-    cid: 0,
-    project: '',
-    user: '',
-    link: '',
-    mask: '',
-    network: '',
-    helpdesk: '',
-    data: {},
-    attribute: 0,
-  }),
-  getters: {
-    getComputer: (state) => state,
-    getLink: (state) => state.link,
-  },
-  actions: {
-    async computerInfo() {
-      const uiStore = useUiStore()
+export const useComputerStore = defineStore('computer', () => {
+  const name = ref('')
+  const uuid = ref('')
+  const cid = ref(0)
+  const project = ref('')
+  const user = ref('')
+  const link = ref('')
+  const mask = ref('')
+  const network = ref('')
+  const helpdesk = ref('')
+  const data = ref({})
+  const attribute = ref(0)
 
+  async function computerInfo() {
+    const uiStore = useUiStore()
+
+    await api
+      .get(`${internalApi}/preferences/server`)
+      .then((response) => {
+        uuid.value = response.data.uuid
+        name.value = response.data.computer_name
+        user.value = response.data.user
+        project.value = response.data.project
+      })
+      .catch((error) => {
+        uiStore.notifyError(error)
+      })
+  }
+
+  async function computerNetwork() {
+    const uiStore = useUiStore()
+
+    await api
+      .get(`${internalApi}/computer/network`)
+      .then((response) => {
+        mask.value = response.data.mask
+        network.value = response.data.network
+      })
+      .catch((error) => {
+        uiStore.notifyError(error)
+      })
+  }
+
+  async function computerId() {
+    const appStore = useAppStore()
+    const uiStore = useUiStore()
+
+    const { clientVersion, protocol, host } = storeToRefs(appStore)
+
+    if (clientVersion.value.startsWith('4.')) {
       await api
-        .get(`${internalApi}/preferences/server`)
+        .get(
+          `${protocol.value}://${host.value}/get_computer_info/?uuid=${uuid.value}`
+        )
         .then((response) => {
-          this.uuid = response.data.uuid
-          this.name = response.data.computer_name
-          this.user = response.data.user
-          this.project = response.data.project
+          cid.value = response.data.id
+          helpdesk.value = response.data.helpdesk
+          setComputerLink()
         })
         .catch((error) => {
           uiStore.notifyError(error)
         })
-    },
-
-    async computerNetwork() {
-      const uiStore = useUiStore()
-
+    } else {
       await api
-        .get(`${internalApi}/computer/network`)
+        .get(`${internalApi}/computer/id`)
         .then((response) => {
-          this.mask = response.data.mask
-          this.network = response.data.network
+          cid.value = response.data
+          setComputerLink()
         })
         .catch((error) => {
           uiStore.notifyError(error)
         })
-    },
+    }
+  }
 
-    async computerId() {
-      const appStore = useAppStore()
-      const uiStore = useUiStore()
+  async function computerLabel() {
+    const appStore = useAppStore()
+    const uiStore = useUiStore()
 
-      if (appStore.clientVersion.startsWith('4.')) {
-        await api
-          .get(
-            `${appStore.protocol}://${appStore.host}/get_computer_info/?uuid=${this.uuid}`
-          )
-          .then((response) => {
-            this.cid = response.data.id
-            this.setComputerLink()
-          })
-          .catch((error) => {
-            uiStore.notifyError(error)
-          })
-      } else {
-        await api
-          .get(`${internalApi}/computer/id`)
-          .then((response) => {
-            this.cid = response.data
-            this.setComputerLink()
-          })
-          .catch((error) => {
-            uiStore.notifyError(error)
-          })
-      }
-    },
+    const { initialUrl, token } = storeToRefs(appStore)
 
-    async computerData() {
-      const appStore = useAppStore()
-      const uiStore = useUiStore()
+    if (cid.value)
+      await api
+        .get(
+          `${initialUrl.value.token}${tokenApi.computer}${cid.value}/label/`,
+          {
+            headers: { Authorization: token.value },
+          }
+        )
+        .then((response) => {
+          helpdesk.value = response.data.helpdesk
+        })
+        .catch((error) => {
+          uiStore.notifyError(error)
+        })
+  }
 
-      if (this.cid)
-        await api
-          .get(`${appStore.initialUrl.token}${tokenApi.computer}${this.cid}/`, {
-            headers: { Authorization: appStore.token },
-          })
-          .then((response) => {
-            this.data = response.data
-          })
-          .catch((error) => {
-            uiStore.notifyError(error)
-          })
-    },
+  async function computerData() {
+    const appStore = useAppStore()
+    const uiStore = useUiStore()
 
-    async computerAttribute() {
-      const appStore = useAppStore()
-      const uiStore = useUiStore()
+    const { initialUrl, token } = storeToRefs(appStore)
 
-      if (this.cid)
-        await api
-          .get(
-            `${appStore.initialUrl.token}${tokenApi.cidAttribute}${this.cid}`,
-            { headers: { Authorization: appStore.token } }
-          )
-          .then((response) => {
-            if (response.data.count === 1)
-              this.attribute = response.data.results[0].id
-          })
-          .catch((error) => {
-            uiStore.notifyError(error)
-          })
-    },
+    if (cid.value)
+      await api
+        .get(`${initialUrl.value.token}${tokenApi.computer}${cid.value}/`, {
+          headers: { Authorization: token.value },
+        })
+        .then((response) => {
+          data.value = response.data
+        })
+        .catch((error) => {
+          uiStore.notifyError(error)
+        })
+  }
 
-    setComputerLink() {
-      const appStore = useAppStore()
+  async function computerAttribute() {
+    const appStore = useAppStore()
+    const uiStore = useUiStore()
 
-      if (appStore.serverVersion.startsWith('4.'))
-        this.link = `${appStore.protocol}://${appStore.host}/admin/server/computer/${this.cid}/change/`
-      else
-        this.link = `${appStore.protocol}://${appStore.host}/computers/results/${this.cid}/`
-    },
-  },
+    const { initialUrl, token } = storeToRefs(appStore)
+
+    if (cid.value)
+      await api
+        .get(`${initialUrl.value.token}${tokenApi.cidAttribute}${cid.value}`, {
+          headers: { Authorization: token.value },
+        })
+        .then((response) => {
+          if (response.data.count === 1)
+            attribute.value = response.data.results[0].id
+        })
+        .catch((error) => {
+          uiStore.notifyError(error)
+        })
+  }
+
+  function setComputerLink() {
+    const appStore = useAppStore()
+    const { serverVersion, protocol, host } = storeToRefs(appStore)
+
+    if (serverVersion.value.startsWith('4.'))
+      link.value = `${protocol.value}://${host.value}/admin/server/computer/${cid.value}/change/`
+    else
+      link.value = `${protocol.value}://${host.value}/computers/results/${cid.value}/`
+  }
+
+  return {
+    name,
+    uuid,
+    cid,
+    project,
+    user,
+    link,
+    mask,
+    network,
+    helpdesk,
+    data,
+    attribute,
+    computerInfo,
+    computerNetwork,
+    computerId,
+    computerLabel,
+    computerData,
+    computerAttribute,
+  }
 })
