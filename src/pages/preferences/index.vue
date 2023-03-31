4b51d7f0abc8f98d2d625d2555b3bb28172e914b
jact <jachavar@gmail.com>
2023-03-31 09:59:57 +0200

    feat: added update preferences action

--- src/pages/preferences/index.vue
+++ src/pages/preferences/index.vue
@@ -2,17 +2,38 @@
   <q-page padding>
     <q-breadcrumbs>
       <q-breadcrumbs-el :label="$gettext('Preferences')" icon="mdi-cog" />
+      <q-btn
+        icon="mdi-sync"
+        size="sm"
+        flat
+        color="primary"
+        :loading="isUpdating"
+        :disabled="isUpdating"
+        @click="updatePreferences"
+        >
+        <q-tooltip>{{ $gettext('Update') }}</q-tooltip></q-btn
+      >
     </q-breadcrumbs>
 
-    <Preferences />
+    <div v-if="isUpdating" class="row q-ma-xl">
+      <div class="col-12 text-center">
+        <q-spinner color="primary" size="6em" />
+      </div>
+    </div>
+
+    <Preferences v-else />
   </q-page>
 </template>
 
 <script>
+import { storeToRefs } from 'pinia'
 import { useGettext } from 'vue3-gettext'
 import { useMeta } from 'quasar'
 
 import Preferences from 'components/Preferences.vue'
+
+import { usePreferencesStore } from 'src/stores/preferences'
+import { useUiStore } from 'src/stores/ui'
 
 export default {
   components: {
@@ -21,7 +42,20 @@ export default {
   setup() {
     const { $gettext } = useGettext()
 
+    const preferencesStore = usePreferencesStore()
+    const uiStore = useUiStore()
+
+    const { isUpdating } = storeToRefs(uiStore)
+
     useMeta({ title: $gettext('Preferences') })
+
+    const updatePreferences = async () => {
+      uiStore.updating()
+      await preferencesStore.readPreferences()
+      uiStore.updatingFinished()
+    }
+
+    return { isUpdating, updatePreferences }
   },
 }
 </script>
