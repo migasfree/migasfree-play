599f4bab94231fa6176595f527f37f74d8f473f2
jact <jachavar@gmail.com>
2025-01-23 08:01:25 +0100

    feat: usage of BannerInfo component

--- src/components/Executions.vue
+++ src/components/Executions.vue
@@ -10,24 +10,22 @@
       :icon="item.icon || ''"
     />
   </q-list>
-  <q-banner v-else class="bg-info text-black q-ma-md">
-    <template #avatar>
-      <q-icon name="mdi-information-outline" color="white" />
-    </template>
-    {{ $gettext('There are not items to show.') }}
-  </q-banner>
+
+  <BannerInfo v-else :message="$gettext('There are not items to show.')" />
 </template>
 
 <script>
 import { storeToRefs } from 'pinia'
 
-import ExecutionDetail from 'components/ExecutionDetail.vue'
+import BannerInfo from 'components/BannerInfo'
+import ExecutionDetail from 'components/ExecutionDetail'
 
 import { useExecutionsStore } from 'src/stores/executions'
 
 export default {
   name: 'Executions',
   components: {
+    BannerInfo,
     ExecutionDetail,
   },
   setup() {
