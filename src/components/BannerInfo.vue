f5ff08c258254f9ce55b0bcb70086ab2d6ebaa1e
jact <jachavar@gmail.com>
2025-01-23 08:00:50 +0100

    feat: added BannerInfo component

---
+++ src/components/BannerInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-banner class="bg-light-blue-3 text-black q-ma-md">
+    <template #avatar>
+      <q-icon name="mdi-information-outline" color="black" />
+    </template>
+    {{ message }}
+  </q-banner>
+</template>
+
+<script>
+export default {
+  name: 'BannerInfo',
+
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
