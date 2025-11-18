2640b10de9025bebc86e53c691533c079e4c8736
jact <jachavar@gmail.com>
2025-11-18 18:42:46 +0100

    refactor: code

--- src/api/routes/tags.js
+++ src/api/routes/tags.js
@@ -3,46 +3,34 @@ import { pythonExecute } from '../utils.js'
 
 const router = express.Router()
 
-router.get('/available', (req, res) => {
-  let code = `
-import json
-from migasfree_client.tags import MigasFreeTags
-
-print(json.dumps(MigasFreeTags().get_available_tags()))`
-
-  if (req.query.version.startsWith('4.'))
-    code = `
+const buildCode = (action, version) => {
+  return version.startsWith('4.')
+    ? `
 import json
 from migasfree_client.tags import MigasFreeTags
 
 mft = MigasFreeTags()
 response = mft._get_tags()
-print(json.dumps(response['available'], ensure_ascii=False))`
-
-  pythonExecute(res, code, 'application/json').then((results) =>
-    res.send(results),
-  )
-})
-
-router.get('/assigned', (req, res) => {
-  let code = `
+print(json.dumps(response['${action}'], ensure_ascii=False))`
+    : `
 import json
 from migasfree_client.tags import MigasFreeTags
 
-print(json.dumps(MigasFreeTags().get_assigned_tags()))`
+print(json.dumps(MigasFreeTags().${action === 'available' ? 'get_available_tags' : 'get_assigned_tags'}()))`
+}
 
-  if (req.query.version.startsWith('4.'))
-    code = `
-import json
-from migasfree_client.tags import MigasFreeTags
+const handleTag = async (req, res, action) => {
+  try {
+    const code = buildCode(action, req.query.version || '')
+    const results = await pythonExecute(res, code, 'application/json')
+    res.send(results)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Failed to execute Python code' })
+  }
+}
 
-mft = MigasFreeTags()
-response = mft._get_tags()
-print(json.dumps(response['selected'], ensure_ascii=False))`
-
-  pythonExecute(res, code, 'application/json').then((results) =>
-    res.send(results),
-  )
-})
+router.get('/available', (req, res) => handleTag(req, res, 'available'))
+router.get('/assigned', (req, res) => handleTag(req, res, 'selected'))
 
 export default router
