ee513eda597e69fd27de1f2a5715e1c06d80a55b
jact <jachavar@gmail.com>
2025-11-21 09:15:07 +0100

    refactor: unified endpoints (avoid repeat)

--- src/api/routes/tags.js
+++ src/api/routes/tags.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import { pythonExecute } from '../utils.js'
+import { debug, pythonExecute } from '../utils.js'
 
 const router = express.Router()
 
-const buildCode = (action, version) => {
+const buildCode = (version) => {
   return version.startsWith('4.')
     ? `
 import json
@@ -11,26 +11,35 @@ from migasfree_client.tags import MigasFreeTags
 
 mft = MigasFreeTags()
 response = mft._get_tags()
-print(json.dumps(response['${action}'], ensure_ascii=False))`
+print(json.dumps({
+  "assigned": response["selected"],
+  "available": response["available"]
+}, ensure_ascii=False))`
     : `
 import json
 from migasfree_client.tags import MigasFreeTags
 
-print(json.dumps(MigasFreeTags().${action === 'available' ? 'get_available_tags' : 'get_assigned_tags'}()))`
+mft = MigasFreeTags()
+print(json.dumps({
+  "assigned": mft.get_assigned_tags(),
+  "available": mft.get_available_tags()
+}, ensure_ascii=False))`
 }
 
-const handleTag = async (req, res, action) => {
+router.get('/', async (req, res) => {
+  const version = req.query.version || ''
+  if (debug) {
+    console.log(`[express] Getting tags (${version})...`)
+  }
+
+  const code = buildCode(version)
   try {
-    const code = buildCode(action, req.query.version || '')
     const results = await pythonExecute(res, code, 'application/json')
     res.send(results)
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Failed to execute Python code' })
+    if (debug) console.error(error)
+    res.status(500).json({ assigned: [], available: {} })
   }
-}
-
-router.get('/available', (req, res) => handleTag(req, res, 'available'))
-router.get('/assigned', (req, res) => handleTag(req, res, 'selected'))
+})
 
 export default router
