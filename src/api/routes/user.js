cdf60655541486dc2c87452f6a4ccfe79f3097ee
jact <jachavar@gmail.com>
2025-11-21 10:23:14 +0100

    refactor: async/await

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -3,7 +3,7 @@ import { pythonExecute, debug } from '../utils.js'
 
 const router = express.Router()
 
-router.post('/check', (req, res) => {
+router.post('/check', async (req, res) => {
   if (debug) console.log('[express] Checking user...')
 
   const code = `
@@ -80,9 +80,13 @@ elif platform.system() == "Linux":
 print(is_privileged)
 `
 
-  pythonExecute(res, code, 'application/json').then((results) =>
-    res.send({ is_privileged: results === 'True' }),
-  )
+  try {
+    const results = await pythonExecute(res, code, 'application/json')
+    res.send({ is_privileged: results === 'True' })
+  } catch (error) {
+    if (debug) console.error(error)
+    res.status(500).json({ is_privileged: false })
+  }
 })
 
 export default router
