dd6929250d5c497cc644ccdf7573c3714d67a15d
jact <jachavar@gmail.com>
2025-12-26 13:24:17 +0100

    feat: added component to avoid DRY

---
+++ src/components/FilterCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <q-card class="q-mx-sm q-my-lg" flat bordered>
+        <q-card-section>
+          <slot />
+        </q-card-section>
+
+        <q-card-actions v-if="$slots.actions" align="right">
+          <slot name="actions" />
+        </q-card-actions>
+      </q-card>
+    </div>
+  </div>
+</template>
