065f902c644ac9a564b5498a63fe4723c05917e2
jact <jachavar@gmail.com>
2025-12-26 13:41:29 +0100

    feat: added component to avoid DRY

---
+++ src/components/InfoItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-item v-if="show">
+    <q-item-section avatar>
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section class="text-h6">
+      <slot>{{ label }}</slot>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup>
+defineProps({
+  icon: { type: String, required: true },
+  label: { type: String, default: '' },
+  show: { type: Boolean, default: true },
+})
+</script>
