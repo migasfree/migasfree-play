a4d600962ce6ec44f6f02cfe07f2a7e7c453eba9
jact <jachavar@gmail.com>
2025-12-26 13:27:01 +0100

    refactor: PageLayout usage

--- src/pages/info/index.vue
+++ src/pages/info/index.vue
@@ -1,74 +1,46 @@
 <template>
   <q-page padding>
-    <PageHeader :title="$gettext('Info')" icon="info" class="print-hide">
-      <q-btn
-        icon="mdi-sync"
-        size="sm"
-        flat
-        color="primary"
-        :loading="isUpdating"
-        :disabled="isUpdating"
-        @click="updateInfo"
-      >
-        <q-tooltip>{{ $gettext('Update') }}</q-tooltip></q-btn
-      >
-    </PageHeader>
-
-    <div v-if="isUpdating" class="row q-ma-xl">
-      <div class="col-12 text-center">
-        <q-spinner color="primary" size="6em" />
-      </div>
-    </div>
-
-    <Info v-else />
+    <PageLayout
+      :title="$gettext('Info')"
+      icon="info"
+      class="print-hide"
+      :show-count="false"
+      :loading="isUpdating"
+      @sync="sync"
+    >
+      <template #content>
+        <Info />
+      </template>
+    </PageLayout>
   </q-page>
 </template>
 
-<script>
-import { storeToRefs } from 'pinia'
+<script setup>
 import { useGettext } from 'vue3-gettext'
 import { useMeta } from 'quasar'
 
 import Info from 'components/Info'
-import PageHeader from 'components/PageHeader'
+import PageLayout from 'components/PageLayout'
 
 import { useComputerStore } from 'src/stores/computer'
 import { usePackagesStore } from 'src/stores/packages'
-import { useUiStore } from 'src/stores/ui'
+import { usePageSync } from 'src/composables/usePageSync'
 
-export default {
-  components: {
-    Info,
-    PageHeader,
-  },
-  setup() {
-    const { $gettext } = useGettext()
+const { $gettext } = useGettext()
 
-    const computerStore = useComputerStore()
-    const packagesStore = usePackagesStore()
-    const uiStore = useUiStore()
+const computerStore = useComputerStore()
+const packagesStore = usePackagesStore()
 
-    const { isUpdating } = storeToRefs(uiStore)
+useMeta({ title: $gettext('Info') })
 
-    useMeta({ title: $gettext('Info') })
-
-    const updateInfo = async () => {
-      uiStore.updating()
-      try {
-        await Promise.all([
-          computerStore.computerInfo(),
-          computerStore.computerNetwork(),
-          computerStore.computerId(),
-          computerStore.computerData(),
-          computerStore.computerAttribute(),
-          packagesStore.setInventory(),
-        ])
-      } finally {
-        uiStore.updatingFinished()
-      }
-    }
-
-    return { isUpdating, updateInfo }
-  },
-}
+const { isUpdating, sync } = usePageSync(async () => {
+  await Promise.all([
+    computerStore.computerInfo(),
+    computerStore.computerNetwork(),
+    computerStore.computerId(),
+    computerStore.computerData(),
+    computerStore.computerAttribute(),
+    packagesStore.setInventory(),
+  ])
+})
 </script>
