0ed4a2ee629cae86db6528811a24be1d6206c56e
jact <jachavar@gmail.com>
2025-12-26 13:24:50 +0100

    refactor: FilterCard usage

--- src/components/AppFilter.vue
+++ src/components/AppFilter.vue
@@ -1,75 +1,58 @@
 <template>
-  <div class="row">
-    <div class="col">
-      <q-card class="q-ma-sm" flat>
-        <q-card-section>
-          <p>
-            <q-input
-              v-model="searchApp"
-              :placeholder="$gettext('Search in name or description')"
-              clearable
-              autofocus
-              @update:model-value="appsStore.filterApps"
-              ><template #prepend><q-icon name="mdi-magnify" /></template
-            ></q-input>
-          </p>
+  <FilterCard>
+    <p>
+      <q-input
+        v-model="searchApp"
+        :placeholder="$gettext('Search in name or description')"
+        clearable
+        autofocus
+        @update:model-value="appsStore.filterApps"
+        ><template #prepend><q-icon name="mdi-magnify" /></template
+      ></q-input>
+    </p>
 
-          <p>
-            <q-select
-              v-model="selectedCategory"
-              :label="$gettext('Category')"
-              :options="categories"
-              option-value="id"
-              option-label="name"
-              clearable
-              @update:model-value="appsStore.filterApps"
-            />
-          </p>
+    <p>
+      <q-select
+        v-model="selectedCategory"
+        :label="$gettext('Category')"
+        :options="categories"
+        option-value="id"
+        option-label="name"
+        clearable
+        @update:model-value="appsStore.filterApps"
+      />
+    </p>
 
-          <p>
-            <q-toggle
-              v-model="onlyInstalledApps"
-              :label="
-                onlyInstalledApps
-                  ? $gettext('View All Apps')
-                  : $gettext('View Installed Apps')
-              "
-              checked-icon="mdi-download"
-              unchecked-icon="mdi-select-all"
-              size="xl"
-              :false-value="false"
-              :true-value="true"
-              @update:model-value="appsStore.filterApps"
-            />
-          </p>
-        </q-card-section>
-      </q-card>
-    </div>
-  </div>
+    <p>
+      <q-toggle
+        v-model="onlyInstalledApps"
+        :label="
+          onlyInstalledApps
+            ? $gettext('View All Apps')
+            : $gettext('View Installed Apps')
+        "
+        checked-icon="mdi-download"
+        unchecked-icon="mdi-select-all"
+        size="xl"
+        :false-value="false"
+        :true-value="true"
+        @update:model-value="appsStore.filterApps"
+      />
+    </p>
+  </FilterCard>
 </template>
 
-<script>
+<script setup>
 import { storeToRefs } from 'pinia'
+
+import FilterCard from 'components/FilterCard'
 
 import { useAppsStore } from 'src/stores/apps'
 import { useFiltersStore } from 'src/stores/filters'
 
-export default {
-  name: 'AppFilter',
-  setup() {
-    const appsStore = useAppsStore()
-    const filtersStore = useFiltersStore()
+const appsStore = useAppsStore()
+const filtersStore = useFiltersStore()
 
-    const { searchApp, selectedCategory, categories, onlyInstalledApps } =
-      storeToRefs(filtersStore)
-
-    return {
-      searchApp,
-      selectedCategory,
-      categories,
-      onlyInstalledApps,
-      appsStore,
-    }
-  },
-}
+const { searchApp, selectedCategory, categories, onlyInstalledApps } =
+  storeToRefs(filtersStore)
 </script>
