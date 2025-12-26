f45dc8a711502ba388cee6223c666c4361cbf11c
jact <jachavar@gmail.com>
2025-12-26 13:25:01 +0100

    refactor: FilterCard usage

--- src/components/DeviceFilter.vue
+++ src/components/DeviceFilter.vue
@@ -1,55 +1,44 @@
 <template>
-  <div class="row">
-    <div class="col">
-      <q-card class="q-ma-sm" flat>
-        <q-card-section>
-          <p>
-            <q-input
-              v-model="searchDevice"
-              :placeholder="$gettext('Search in name or model or manufacturer')"
-              clearable
-              @update:model-value="devicesStore.filterDevices"
-              ><template #prepend><q-icon name="mdi-magnify" /></template
-            ></q-input>
-          </p>
+  <FilterCard>
+    <p>
+      <q-input
+        v-model="searchDevice"
+        :placeholder="$gettext('Search in name or model or manufacturer')"
+        clearable
+        @update:model-value="devicesStore.filterDevices"
+        ><template #prepend><q-icon name="mdi-magnify" /></template
+      ></q-input>
+    </p>
 
-          <p>
-            <q-toggle
-              v-model="onlyAssignedDevices"
-              :label="
-                onlyAssignedDevices
-                  ? $gettext('View All available')
-                  : $gettext('View Assigned Devices')
-              "
-              checked-icon="mdi-checkbox-marked"
-              unchecked-icon="mdi-select"
-              size="xl"
-              :false-value="false"
-              :true-value="true"
-              @update:model-value="devicesStore.filterDevices"
-            />
-          </p>
-        </q-card-section>
-      </q-card>
-    </div>
-  </div>
+    <p>
+      <q-toggle
+        v-model="onlyAssignedDevices"
+        :label="
+          onlyAssignedDevices
+            ? $gettext('View All available')
+            : $gettext('View Assigned Devices')
+        "
+        checked-icon="mdi-checkbox-marked"
+        unchecked-icon="mdi-select"
+        size="xl"
+        :false-value="false"
+        :true-value="true"
+        @update:model-value="devicesStore.filterDevices"
+      />
+    </p>
+  </FilterCard>
 </template>
 
-<script>
+<script setup>
 import { storeToRefs } from 'pinia'
+
+import FilterCard from 'components/FilterCard'
 
 import { useDevicesStore } from 'src/stores/devices'
 import { useFiltersStore } from 'src/stores/filters'
 
-export default {
-  name: 'DeviceFilter',
-  setup() {
-    const devicesStore = useDevicesStore()
-    const filtersStore = useFiltersStore()
+const devicesStore = useDevicesStore()
+const filtersStore = useFiltersStore()
 
-    const { searchDevice, onlyAssignedDevices } = storeToRefs(filtersStore)
-
-    return { devicesStore, searchDevice, onlyAssignedDevices }
-  },
-}
+const { searchDevice, onlyAssignedDevices } = storeToRefs(filtersStore)
 </script>
