dc6fcd5e12acbc821c8dc80c44ffa333e787d6d4
jact <jachavar@gmail.com>
2026-01-23 12:45:14 +0100

    docs(ipc): add JSDoc to user handlers

--- src-electron/handlers/user.js
+++ src-electron/handlers/user.js
@@ -2,7 +2,18 @@ import path from 'path'
 import { ipcMain } from 'electron'
 import { pythonExecute, debug, getScriptsPath } from '../python-utils.js'
 
+/**
+ * Registers IPC handlers related to user actions.
+ */
 export default function registerUserHandlers() {
+  /**
+   * Checks if the given user has privileged access.
+   * @param {Object} _ - Unused event object.
+   * @param {Object} params - Parameters.
+   * @param {string} params.username - The username to check.
+   * @param {string} params.password - The password for verification.
+   * @returns {Promise<Object>} Object containing the privilege status.
+   */
   ipcMain.handle('user:check', async (_, { username, password }) => {
     if (debug) console.log('[ipc] Checking user...')
 
